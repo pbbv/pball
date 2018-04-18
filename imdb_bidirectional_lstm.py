1198b3eab1e7de6f033cfe74e1e9cac7c8a9cdda
pbbv <pnawbif@gmail.com>
2018-04-18 05:54:41 +0300

    1

---
+++ imdb_bidirectional_lstm.py
@@ -0,0 +1,71 @@
+'''Trains a Bidirectional LSTM on the IMDB sentiment classification task.
+
+Output after 4 epochs on CPU: ~0.8146
+Time per epoch on CPU (Core i7): ~150s.
+'''
+
+from __future__ import print_function
+import numpy as np
+
+from keras.preprocessing import sequence
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Embedding, LSTM, Bidirectional
+from keras.datasets import imdb
+
+
+max_features = 20000
+# cut texts after this number of words
+# (among top max_features most common words)
+maxlen = 100
+batch_size = 32
+
+print('Loading data...')
+(x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=max_features)
+print(len(x_train), 'train sequences')
+print(len(x_test), 'test sequences')
+
+print('Pad sequences (samples x time)')
+x_train = sequence.pad_sequences(x_train, maxlen=maxlen)
+x_test = sequence.pad_sequences(x_test, maxlen=maxlen)
+print('x_train shape:', x_train.shape)
+print('x_test shape:', x_test.shape)
+y_train = np.array(y_train)
+y_test = np.array(y_test)
+
+model = Sequential()
+model.add(Embedding(max_features, 128, input_length=maxlen))
+model.add(Bidirectional(LSTM(64)))
+model.add(Dropout(0.5))
+model.add(Dense(1, activation='sigmoid'))
+
+# try using different optimizers and different optimizer configs
+model.compile('adam', 'binary_crossentropy', metrics=['accuracy'])
+
+print('Train...')
+model.fit(x_train, y_train,
+          batch_size=batch_size,
+          epochs=4,
+          validation_data=[x_test, y_test])
+
+
+
+
+"""
+x250
+Epoch 1/4
+25000/25000 [==============================] - 422s 17ms/step
+loss: 0.4174 - acc: 0.8078 - val_loss: 0.3365 - val_acc: 0.8524
+"""
+
+
+"""
+k80
+loss: 0.6753 - acc: 0.598225000/25000 
+[==============================] -   296s 12ms/step - loss: 0.4137 - acc: 0.8098
+ - val_loss: 0.3482 - val_acc: 0.8502
+
+
+"""
+
+
+
